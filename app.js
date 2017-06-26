0342d88c2a0b9497573ede3ced32f4fa3eca7b70
Alexa Vita <alexvita2@gmail.com>
2017-06-26 11:25:28 -0400

    minor edits, bug fixes

--- app.js
+++ app.js
@@ -82,4 +82,4 @@ app.get('/:encoded_id', function(req, res){
 var server = app.listen(process.env.PORT || 5000, function () {
     var port = server.address().port;
     console.log("App now running on port", port);
-  }
+  })
