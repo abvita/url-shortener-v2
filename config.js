1ee7e7eaf2aad9fcfd416bd5a0f8ad49f109f27f
Alexa Vita <alexvita2@gmail.com>
2017-06-26 11:03:17 -0400

    db troubleshooting

--- config.js
+++ config.js
@@ -3,10 +3,10 @@ var config = {};
 config.db = {};
 // the URL shortening host - shortened URLs will be this + base58 ID
 // i.e.: http://localhost:3000/3Ys
-config.webhost = 'http://localhost:5000/';
+config.webhost = 'https://shortlnk.herokuapp.com/';
 
 // your MongoDB host and database name
-config.db.host = 'https://shortlnk.herokuapp.com/';
+config.db.host = 'shortlnk.herokuapp';
 config.db.name = 'url_shortener';
 
 module.exports = config;
