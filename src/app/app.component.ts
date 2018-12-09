7b509c18b80da2c1aaff9d16fc660a6e3e5d5788
Alexa Vita <alexvita2@gmail.com>
2018-12-09 15:45:03 -0500

    fixed style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,7 @@ import {Http, Response, Headers, RequestOptions } from '@angular/http';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './app.component.html'
 })
 
 export class AppComponent {
