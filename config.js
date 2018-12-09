2fab6d11e8d06232f816dfbee197cac8623cf914
Alexa Vita <alexvita2@gmail.com>
2018-12-09 15:21:19 -0500

    working functionality with angular2 upgrade

---
+++ config.js
@@ -0,0 +1,9 @@
+//Mongo configuration
+var config = {};
+
+config.db = {};
+config.webhost = 'https://shortlnk.herokuapp.com/';
+config.db.host = 'shortlnk.herokuapp';
+config.db.name = 'url_shortener';
+
+module.exports = config;
---
+++ models/url.js
@@ -0,0 +1,41 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+//COUNTER DB
+// create the counters schema with an _id field and a seq field
+var CounterSchema = Schema({
+    _id: {type: String, required: true},
+    seq: { type: Number, default: 0 }
+});
+
+// create a model from that schema
+var counter = mongoose.model('counter', CounterSchema);
+
+
+//URL DB
+// create a schema for our links in url db collection
+var urlSchema = new Schema({
+  _id: {type: Number, index: true},
+  long_url: String,
+  created_at: Date
+});
+
+// The pre('save', callback) middleware executes the callback function
+// every time before an entry is saved to the urls collection.
+urlSchema.pre('save', function(next){
+  var doc = this;
+  // find the url_count and increment it by 1
+  counter.findByIdAndUpdate({_id: 'url_count'}, {$inc: {seq: 1} }, function(error, counter) {
+      if (error)
+          return next(error);
+      // set the _id of the urls collection to the incremented value of the counter
+      doc._id = counter.seq;
+      doc.created_at = new Date();
+      next();
+  });
+});
+
+// create URL model from schema
+var Url = mongoose.model('Url', urlSchema);
+
+module.exports = Url;
