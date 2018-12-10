04ac1c7ee0415d5dda16c4c5138de995a669e024
Alexa Vita <alexvita2@gmail.com>
2018-12-10 15:05:49 -0500

    v2 code update

---
+++ src/app/app.component.ts
@@ -0,0 +1,58 @@
+import { Component } from '@angular/core';
+import {Http, Response, Headers, RequestOptions } from '@angular/http'; 
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html'
+})
+
+export class AppComponent {
+
+  constructor(private http: Http) { }
+
+  title = 'url-shortener';
+  urlData = {
+    long:'',
+    short:''
+  };
+  //background color scope variables
+  bgColor = '';
+  colCount = 0;
+  colorArr = ['oneBg', 'twoBg', 'threeBg', 'fourBg'];
+  currColor = '';
+
+  //URL shortening function
+  shorten(): any {
+    let formattedUrl = this.formatUrl(this.urlData.long);
+    let request = {url: formattedUrl};
+    this.http.post('/api/shorten', request).subscribe(
+            (response: any) => {
+              let resp = JSON.parse(response._body);
+              var resultHTML = '<a href="' + resp.shortUrl + '">'
+                  + resp.shortUrl + '</a>';
+              this.urlData.short = resp.shortUrl;
+          },
+          (error: any) => {
+            console.log(error);
+          });
+
+    //background color cycle script
+    this.colCount ++;
+      if (this.colCount < this.colorArr.length)
+      {
+          this.bgColor = this.colorArr[this.colCount];
+      } else {
+          this.colCount = 0;
+          this.bgColor = this.colorArr[this.colCount];
+      }
+  }
+
+  formatUrl(data: any): any {
+    if (data.toLowerCase().includes('http://') || data.toLowerCase().includes('https://')) {
+      return data;
+    } else {
+      let formattedData = 'http://' + data;
+      return formattedData;
+    }
+  }
+}
