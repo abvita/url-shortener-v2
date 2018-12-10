e3adba785ea96491cabdb26a1d9e14470f0c551d
Alexa Vita <alexvita2@gmail.com>
2018-12-09 22:51:10 -0500

    rever to v1 code

---
+++ public/javascript/myApp.js
@@ -0,0 +1,35 @@
+var myApp = angular.module('myApp', []);
+
+myApp.controller('myController', function($scope, $http) {
+  $scope.urlData = {};
+  //background color scope variables
+  $scope.bgColor = '';
+  var colCount = 0;
+  var colorArr = ['oneBg', 'twoBg', 'threeBg', 'fourBg'];
+  var currColor = '';
+
+  //URL shortening function
+  $scope.shorten = function() {
+    $http({
+          method  : 'POST',
+          url     : '/api/shorten',
+          data    : {url: $('#url-field').val()}
+         })
+          .then(function(response) {
+              var resultHTML = '<a href="' + response.data.shortUrl + '">'
+                  + response.data.shortUrl + '</a>';
+              $scope.urlData.short = response.data.shortUrl;
+          });
+
+    //background color cycle script
+    colCount ++;
+      if (colCount < colorArr.length)
+      {
+          $scope.bgColor = colorArr[colCount];
+      } else {
+          colCount = 0;
+          $scope.bgColor = colorArr[colCount];
+      }
+  }
+
+});
